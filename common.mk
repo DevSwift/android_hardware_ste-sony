34e114fdf1f49f76f79a9eae058d568d59f07d3a
DevSwift <devswift@devswift-R520-R522-R620.(none)>
2014-04-18 19:17:21 +0200

    more stuff

---
+++ common.mk
@@ -0,0 +1,80 @@
+# Audio
+PRODUCT_PACKAGES += \
+   audio_policy.montblanc \
+   audio.primary.montblanc \
+   libasound \
+   libalsactrl \
+   libmmprobe
+
+# STE Display 
+PRODUCT_PACKAGES += \
+   lights.montblanc \
+   gralloc.montblanc \
+   copybit.montblanc
+
+# STE Media
+PRODUCT_PACKAGES += \
+   libstagefrighthw \
+   libomxil-bellagio \
+   libstelpcutils \
+   libtrace
+
+# WLAN
+PRODUCT_PACKAGES += \
+   libnetcmdiface \
+   libnl_2
+
+# Modem
+PRODUCT_PACKAGES += \
+   ste_mad \
+   msa \
+   libisimessage \
+   libmalat \
+   libmalcs \
+   libmalftd \
+   libmalgpds \
+   libmalgss \
+   libmalmce \
+   libmalmis \
+   libmalmon \
+   libmalmte \
+   libmalnet \
+   libmalnvd \
+   libmalpipe \
+   libmalrf \
+   libmalsms \
+   libmaluicc \
+   libmalutil \
+   libshmnetlnk \
+   libphonet
+
+# cspsa
+PRODUCT_PACKAGES += \
+   cspsa-server \
+   libcspsa \
+   cspsa.conf \
+   cspsa-cmd \
+   libcspsa-core \
+   libcspsa-ll-file
+
+# cg2900
+PRODUCT_PACKAGES += \
+   ste-cg29xx_ctrl
+
+# psdata
+PRODUCT_PACKAGES += \
+   libstecom \
+   libmpl \
+   libpscc \
+   libsterc \
+   stercd
+
+PRODUCT_PACKAGES += \
+   chargemode \
+   dbus-daemon \
+   watchdog-kicker
+
+PRODUCT_PACKAGES += \
+   libdbus \
+   libarchive \
+   libarchive_fe
